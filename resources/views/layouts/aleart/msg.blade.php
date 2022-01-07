a488fd35e5b25a16f8b01a1991881ed9446c4c54
Sajana Karunarathna <66436602+SajanaAshen@users.noreply.github.com>
2022-01-07 14:38:51 +0530

    session

--- resources/views/layouts/aleart/msg.blade.php
+++ resources/views/layouts/aleart/msg.blade.php
@@ -1,9 +1,12 @@
 @php
     if(Session::has('success')){
-        toast('Successful!','success');
+        toast('Successful!','success')->timerProgressBar()->width('300px');
+        Session::forget('success');
     }
 
     if(Session::has('error')){
-        toast(session::get('error'),session::get('error_type'));
+        toast(session::get('error'),session::get('error_type'))->timerProgressBar()->autoClose(10000);
+        Session::forget('error');
+        Session::forget('error_type');
     }
 @endphp
