f1b0b9c9608995590f6c421961b36e4ea471b4ea
Ayesh Nawawickrama <nawawickrama@gmail.com>
2022-05-13 22:49:31 +0530

    Update home.blade.php

--- resources/views/admin/general/home.blade.php
+++ resources/views/admin/general/home.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-12">
         <div class="card">
             <div class="card-header bg-primary text-white">
-                <p>sCPF Link</p>
+                <p>CPF Link</p>
             </div>
             <div class="card-body">
                 @php
