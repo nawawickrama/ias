dac6dfa3c14c3f8cc9d40669ffe2e9b87873cee4
Ayesh Nawawickrama <nawawickrama@gmail.com>
2022-06-10 14:35:38 +0530

    Update email.blade.php

--- resources/views/admin/assessment/email.blade.php
+++ resources/views/admin/assessment/email.blade.php
@@ -99,7 +99,7 @@
 </div>
 <div class="row">
     <div class="column1">
-        <img src="public/assets/images/txt.png" alt="image loading.." width="30%" height="30%">
+        <img src="{{ url('assets/images/txt.png') }}" alt="image loading.." width="30%" height="30%">
         <p class="arial"><b>Datum :</b> {{ $status_date }}, Unterschrift/Stempel prufer(n).</p>
     </div>
 </div>
